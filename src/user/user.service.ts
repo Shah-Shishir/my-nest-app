d81f709f65c9dadde6898dc952d9b7f323b86e73
Shah Newaj Rabbi Shishir <newaj.shishir@selisegroup.com>
2026-04-18 02:37:09 +0600

    update on service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  private users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser: User = {
+      id: this.users.length + 1,
+      username: createUserDto.username,
+      createdAt: new Date()
+    }
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const foundUser = this.users.find((user) => id === +user.id);
+
+    if (!foundUser) {
+      return new NotFoundException();
+    }
+
+    return foundUser;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const foundUser = this.users.find((user) => id === +user.id);
+
+    if (!foundUser) {
+      return new NotFoundException();
+    }
+
+    return {
+      ...foundUser,
+      username: updateUserDto.username,
+      message: 'User has been updated successfully',
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const foundUser = this.users.find((user) => id === +user.id);
+
+    if (!foundUser) {
+      return new NotFoundException();
+    }
+
+    this.users = this.users.filter((user) => id === +user.id);
+
+    return {
+      message: 'User has been deleted successfully',
+    };
   }
 }

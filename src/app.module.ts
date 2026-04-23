eb92bbfe4952e5a369f6b7be108e090223f4d940
Shah Newaj Rabbi Shishir <newaj.shishir@selisegroup.com>
2026-04-24 00:25:40 +0600

    add resource for tenant, branch and category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TenantModule } from './tenant/tenant.module';
+import { BranchModule } from './category/branch/branch.module';
+import { CategoryModule } from './category/category.module';
 import config from '../orm-config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), UserModule],
+  imports: [TypeOrmModule.forRoot(config), UserModule, TenantModule, BranchModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
